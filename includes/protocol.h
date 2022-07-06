6af5b19c39dc0dc1d085baba5272a771d2e0235e
fram112 <francescoamodeo99@gmail.com>
2022-07-06 23:51:22 +0200

    added request, response and error codes

--- includes/protocol.h
+++ includes/protocol.h
@@ -1,33 +1,50 @@
 #ifndef FILE_STORAGE_SERVER_PROTOCOL_H
 #define FILE_STORAGE_SERVER_PROTOCOL_H
 
+#include "conn.h"
+#include "util.h"
+
 typedef enum request_code {
     OPEN,
     READ,
-    WRITE
+    READN,
+    WRITE,
+    APPEND,
+    LOCK,
+    UNLOCK,
+    CLOSE,
+    REMOVE
 } request_c;
 
 typedef enum response_code {
-    INVALID,
-    UNKNOWN,
+
+    //api success response
+    OPEN_FILE_SUCCESS = 200,
+    READ_FILE_SUCCESS = 201,
+    READN_FILE_SUCCESS = 202,
+    WRITE_FILE_SUCCESS = 203,
+    APPEND_FILE_SUCCESS = 204,
+    LOCK_FILE_SUCCESS = 206,
+    UNLOCK_FILE_SUCCESS = 207,
+    CLOSE_FILE_SUCCESS = 208,
+    REMOVE_FILE_SUCCESS = 209,
+    CLOSE_CONN_SUCCESS = 270,
+
+    //client error response
+    FILE_ALREADY_OPEN = 400,
+    FILE_ALREADY_CLOSED = 401,
+    FILE_ALREADY_EXISTS = 402,
+    FILE_NOT_OPENED = 403,
+    FILE_NOT_FOUND = 404,
+    FILE_EMPTY = 405,
+    WRITE_FAILED = 406,
+    INVALID_ARGUMENT = 407,
+    PERMISSION_DENIED = 408,
+
+    //server error response
+    INTERNAL_ERROR = 500
+
 } response_c;
-
-
-//TODO allineamento
-typedef struct request_type {
-    request_c request_code;
-    char *filepath;
-    int flags;
-    int payload_size;
-    char* payload;
-} request_t;
-
-typedef struct response_type {
-    response_c response_code;
-    int request_length;
-    char* request_args;
-} response_t;
-
 
 
 #endif //FILE_STORAGE_SERVER_PROTOCOL_H
