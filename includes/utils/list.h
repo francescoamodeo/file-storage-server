8c1505648fe73d64b706675bc135615dbf946030
fram112 <francescoamodeo99@gmail.com>
2022-07-06 23:40:04 +0200

    added list implementation

---
+++ includes/utils/list.h
@@ -0,0 +1,120 @@
+#ifndef FILE_STORAGE_SERVER_LIST_H
+#define FILE_STORAGE_SERVER_LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+typedef struct node{
+    struct node* previous;
+    void* data;
+    struct node* next;
+}node_t;
+
+typedef struct list{
+    node_t* head;
+    node_t* tail;
+    int length;
+}list_t;
+
+
+/**
+ * Crea una lista di tipo list_t inizializzando tutti i campi.
+ * @return puntatore alla lista creata o NULL in caso di errore. (setta errno)
+ */
+list_t* list_init();
+
+/**
+ * Stampa ogni elemento della lista come array di caratteri. (setta errno)
+ */
+void list_tostring(list_t* list);
+
+/**
+ *  Aggiunge in coda alla lista un elemento con valore data
+ *
+ * @param list - puntatore alla lista a cui aggiungere l'elemento
+ * @param data - puntatore al contenuto generico del nuovo elemento
+ * @return il puntatore all'oggetto appena inserito, NULL in caso di errore. (setta errno)
+ */
+node_t* list_add(list_t* list, void* data);
+
+/**
+ *  Aggiunge in testa alla lista un elemento con valore data
+ *
+ * @param list - puntatore alla lista a cui aggiungere l'elemento
+ * @param data - puntatore al contenuto generico del nuovo elemento
+ * @return il puntatore all'oggetto appena inserito, NULL in caso di errore. (setta errno)
+ */
+node_t* list_addhead(list_t* list, void* data);
+
+/**
+ * Restituisce il puntatore alla testa della lista, senza rimuoverlo
+ *
+ * @param list - lista da cui prelevare il nodo di testa
+ * @return il puntatore al primo nodo della lista, NULL in caso errore. (setta errno)
+ */
+node_t* list_gethead(list_t* list);
+
+/**
+ * Restituisce l'elemento successivo di quello passato come parametro. (setta errno)
+ */
+node_t* list_getnext(list_t* list, node_t* elem);
+
+/**
+ * Restituisce un puntatore al nodo in testa alla lista, rimuovendolo da essa.
+ *
+ * @param list - puntatore alla lista da cui rimuovere il primo nodo
+ * @return puntatore al nodo appena rimosso. (setta errno)
+ */
+node_t* list_removehead(list_t* list);
+
+/**
+ * Restituisce un puntatore all'ultimo nodo della lista, senza rimuoverlo
+ *
+ * @param list - lista da cui prelevare il nodo di coda
+ * @return il puntatore all'ultimo nodo della lista, NULL in caso errore. (setta errno)
+ */
+node_t* list_gettail(list_t* list);
+
+/**
+ * Restituisce un puntatore all'ultimo nodo della lista, rimuovendolo da essa.
+ *
+ * @param list - puntatore alla lista da cui rimuovere l'ultimo nodo
+ * @return puntatore al nodo appena rimosso. (setta errno)
+ */
+node_t* list_removetail(list_t* list);
+
+/**
+ * Confronta il contenuto dei nodi della lista con il valore passato come parametro,
+ * se trova una corrispondenza restituisce un puntatore all'elemento, senza rimuoverlo dalla lista.
+ *
+ * @param list - puntatore alla lista
+ * @param compared - valore da confrontare con il contenuto dei nodi della lista
+ * @param compare_function - funzione utilizzata per confrontare i nodi,
+ * @return puntatore all'elemento trovato, NULL altrimenti. (setta errno)
+ */
+node_t* list_get(list_t* list, void* compared, int (*compare_function)(void*, void*));
+
+/**
+ * Confronta il contenuto dei nodi della lista con il valore passato come parametro,
+ * se trova una corrispondenza restituisce un puntatore all'elemento, rimuovendolo dalla lista,
+ *
+ * @param list - puntatore alla lista
+ * @param value - valore da confrontare con il contenuto dei nodi della lista
+ * @param compare_function - funzione utilizzata per confrontare i nodi,
+ * @return puntatore all'elemento trovato, NULL altrimenti. (setta errno)
+ */
+node_t* list_remove(list_t* list, void* value, int (*compare_function)(void*, void*));
+
+/**
+ * Funzione che dealloca la lista e ogni suo nodo.
+ *
+ * @param list -  puntatore alla lista da deallocare
+ * @param free_content - puntatore alla funzione che dealloca il contenuto dei nodi
+ * (setta errno)
+ */
+void list_destroy(list_t *list);
+
+int compare_int(void *a, void *b);
+
+#endif //FILE_STORAGE_SERVER_LIST_H
---
+++ src/utils/list.c
@@ -0,0 +1,235 @@
+
+#include "list.h"
+
+int compare_int(void *a, void *b){
+    return (*(int*)a) - (*(int*)b);
+}
+
+list_t* list_init() {
+
+    list_t *list = (list_t *)malloc(sizeof(list_t));
+    if(!list) 
+        return NULL;
+    
+    list->head = NULL;
+    list->tail = NULL;
+    list->length = 0;
+
+    return list;
+}
+
+void list_tostring(list_t* list){
+
+    if (!list || list->length == 0) {
+        errno = EINVAL;
+        return;
+    }
+
+    node_t *curr = list->head;
+    while (curr != NULL) {
+        printf("%s\n", (char*)curr->data);
+        curr = curr->next;
+    }
+}
+
+node_t* list_add(list_t* list, void* data){
+
+    if(!list || !data) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    node_t* new = NULL;
+    if((new = (node_t*)malloc(sizeof(node_t))) == NULL)
+        return NULL;
+    
+    new->data = data;
+    new->previous = NULL;
+    new->next = NULL;
+
+    //Se la lista è vuota
+    if(list->length == 0) {
+        list->head = new;
+        list->tail = new;
+    }else{
+        new->previous = list->tail;
+        list->tail->next = new;
+        list->tail = new;
+    }
+    list->length++;
+    return new;
+}
+
+node_t* list_addhead(list_t* list, void* data){
+
+    if(!list || !data) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    node_t* new = NULL;
+    if((new = (node_t*)malloc(sizeof(node_t))) == NULL)
+        return NULL;
+    
+    new->data = data;
+    new->next = NULL;
+    new->previous = NULL;
+
+    //Se la lista è vuota
+    if(list->length == 0) {
+        list->head = new;
+        list->tail = new;
+    }else{
+        new->next = list->head;
+        list->head->previous = new;
+        list->head = new;
+    }
+    list->length++;
+    return new;
+}
+
+node_t* list_gethead(list_t* list){
+
+    if(!list || list->length == 0) {
+        errno = EINVAL;
+        return NULL;
+    }
+    return list->head;
+}
+
+node_t* list_removehead(list_t* list){
+
+    if(!list || list->head == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    node_t *return_elem = list->head;
+    if (list->head == list->tail)
+        list->head = list->tail = NULL;
+    else {
+        list->head = list->head->next;
+        list->head->previous = NULL;
+    }
+    list->length--;
+    return return_elem;
+}
+
+node_t* list_gettail(list_t* list){
+
+    if(!list || list->length == 0) {
+        errno = EINVAL;
+        return NULL;
+    }
+    return list->tail;
+}
+
+node_t* list_removetail(list_t* list){
+
+    if(!list || list->length == 0) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    node_t* return_elem = list->tail;
+    if(list->head == list->tail)
+        list->head = list->tail = NULL;
+    else{
+        list->tail = list->tail->previous;
+        list->tail->next = NULL;
+    }
+    list->length--;
+
+    return return_elem;
+}
+
+node_t* list_get(list_t* list, void* compared, int (*compare_function)(void*, void*)) {
+
+    if (!list || !list->head) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    node_t *curr = list->head;
+    while (curr != NULL) {
+        //Se la compare_function è diversa da NULL uso quella, altrimenti uso uguaglianza semplice
+        if (((compare_function != NULL) && (compare_function(curr->data, compared) == 0)) ||
+            ((compare_function == NULL) && (curr->data == compared))) {
+            //elemento trovato
+            return curr;
+        }
+        curr = curr->next;
+    }
+    errno = EINVAL;
+    return NULL;
+}
+
+node_t* list_remove(list_t* list, void* compared, int (*compare_function)(void*, void*)) {
+
+    if (!list || !list->head) {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    node_t* return_elem = NULL;
+    node_t *curr = list->head;
+    while (curr != NULL) {
+        //Se la compare_function è diversa da NULL uso quella, altrimenti uso una normale uguaglianza
+        if (((compare_function != NULL) && (compare_function(curr->data, compared) == 0)) ||
+            ((compare_function == NULL) && (curr->data == compared))) {
+            //elemento trovato
+            return_elem = curr;
+            //Se è l'elemento di testa
+            if (curr == list->head) {
+                list->head = list->head->next;
+                if (list->head)
+                    list->head->previous = NULL;
+            }
+            //Se è l'elemento di coda
+            else if (curr == list->tail) {
+                list->tail = curr->previous;
+                if (list->tail)
+                    list->tail->next = NULL;
+            }
+            //E' un elemento centrale
+            else {
+                curr->previous->next = curr->next;
+                curr->next->previous = curr->previous;
+            }
+            list->length--;
+            //Se non ci sono più elementi sistemo la coda
+            if (list->length == 0) {
+                list->head = NULL;
+                list->tail = NULL;
+            }
+            return return_elem;
+        }
+        curr = curr->next;
+    }
+    errno = EINVAL;
+    return NULL;
+}
+
+node_t* list_getnext(list_t* list, node_t* elem){
+    if(list == NULL || elem == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+    else return elem->next;
+}
+
+void list_destroy(list_t *list) {
+    if(list == NULL) {
+        errno = EINVAL;
+        return;
+    }
+
+    node_t *tmp;
+    while(list->head != NULL){
+        tmp = list->head;
+        list->head = list->head->next;
+        free(tmp->data);
+        free(tmp);
+    }
+    free(list);
+}
