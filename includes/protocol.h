687794820d3b31e904622b2001eb0d293927f466
fram112 <francescoamodeo99@gmail.com>
2022-07-24 09:09:09 +0200

    protocol improved

--- includes/protocol.h
+++ includes/protocol.h
@@ -4,6 +4,19 @@
 #include "conn.h"
 #include "util.h"
 
+typedef struct header {
+    char pathname[MAX_PATH];
+    char username[MAX_USERNAME];
+    size_t data_size;
+    int code;
+    int arg;
+} msg_header;
+
+typedef struct message {
+    msg_header *header;
+    void *data;
+} msg_t;
+
 typedef enum request_code {
     OPEN,
     READ,
@@ -13,41 +26,132 @@ typedef enum request_code {
     LOCK,
     UNLOCK,
     CLOSE,
-    REMOVE
+    REMOVE,
+    FIN
 } request_c;
 
-typedef enum response_code {
+static inline int writemsg(int to, msg_t* message) {
+    if (to < 0 || !message) {
+        errno = EINVAL;
+        return -1;
+    }
 
-    //api success response
-    OPEN_FILE_SUCCESS = 200,
-    READ_FILE_SUCCESS = 201,
-    READN_FILE_SUCCESS = 202,
-    WRITE_FILE_SUCCESS = 203,
-    APPEND_FILE_SUCCESS = 204,
-    LOCK_FILE_SUCCESS = 206,
-    UNLOCK_FILE_SUCCESS = 207,
-    CLOSE_FILE_SUCCESS = 208,
-    REMOVE_FILE_SUCCESS = 209,
-    CLOSE_CONN_SUCCESS = 210,
+    int wres;
+    if ((wres = writen(to, message->header, sizeof(msg_header))) == -1) return -1;
+    if (message->header->data_size > 0) {
+#if DEBUG
+        printf("scrivo data\n");
+#endif
+        if ((wres = writen(to, message->data, message->header->data_size)) == -1) return -1;
+    }
 
-    //client error response
-    FILE_ALREADY_OPEN = 400,
-    FILE_ALREADY_CLOSED = 401,
-    FILE_ALREADY_EXISTS = 402,
-    FILE_ALREADY_WRITTEN = 403,
-    FILE_NOT_FOUND = 404,
-    FILE_NOT_OPENED = 405,
-    FILE_EMPTY = 406,
-    FILE_TOO_BIG = 407,
-    STORAGE_EMPTY = 408,
-    INVALID_ARGUMENT = 409,
-    PERMISSION_DENIED = 410,
-    FILE_ALREADY_UNLOCKED = 411,
+#if DEBUG
+    printf("messaggio inviato (res: %d)\n", wres);
+#endif
+    return wres;
+}
 
-    //server error response
-    INTERNAL_ERROR = 500
+static inline int readmsg(int from, msg_t *message) {
+    if (from < 0 || !message) {
+        errno = EINVAL;
+        return -1;
+    }
 
-} response_c;
+#if DEBUG
+    printf("leggo il messaggio\n");
+#endif
 
+    int read = 0;
+    if ((read += readn(from, message->header, sizeof(msg_header))) == -1) return -1;
+#if DEBUG
+    printf("letto header %d\n", read);
+    printf("code: %d\n"
+           "flags: %d\n"
+           "filepath: %s\n"
+           "data size: %ld\n",
+           message->header->code, message->header->arg, message->header->pathname, message->header->data_size);
+#endif
+    message->data = NULL;
+    if (message->header->data_size > 0) {
+#if DEBUG
+        printf("data size >0 = %ld\n", message->header->data_size);
+        printf("code readmsg (data)= %d\n", message->header->code);
+#endif
+        message->data = malloc(message->header->data_size);
+        if (!message->data) return -1;
+        if ((read += readn(from, message->data, message->header->data_size)) == -1) {
+            free(message->data);
+            return -1;
+        }
+    }
+#if DEBUG
+    printf("totale messaggio letto %d\n", read);
+#endif
+    return read;
+}
+
+static inline msg_t *buildmsg(char *username, int code, int arg, const char *pathname, size_t data_size, void *data) {
+
+    if (pathname && strlen(pathname) >= MAX_PATH) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (!username || strlen(username) >= MAX_USERNAME){
+        errno = EINVAL;
+        return NULL;
+    }
+
+    msg_t *message = calloc(1, sizeof(msg_t));
+    if (!message) return NULL;
+    message->header = calloc(1, sizeof(msg_header));
+    if (!message->header) return NULL;
+
+    memset(message->header, 0, sizeof(msg_header));
+    if(pathname) {
+        strncpy(message->header->pathname, pathname, strlen(pathname));
+        message->header->pathname[strlen(pathname)] = 0;
+    }
+    strncpy(message->header->username, username, strlen(username));
+    message->header->username[strlen(username)] = 0;
+
+    message->header->code = code;
+    message->header->arg = arg;
+    message->header->data_size = data_size;
+    if (data_size > 0) {
+#if DEBUG
+        printf("imposto data, data size (build message): %ld\n", data_size);
+#endif
+        message->data = malloc(data_size);
+        if (!message->data) return NULL;
+        memcpy(message->data, data, data_size);
+    }
+    return message;
+}
+
+static inline msg_t* initmsg() {
+    msg_t *message = malloc(sizeof(msg_t));
+    if (!message) return NULL;
+    message->header = malloc(sizeof(msg_header));
+    if (!message->header) return NULL;
+
+    message->header->code = -1;
+    message->header->arg = -1;
+    memset(message->header->username, 0, MAX_USERNAME);
+    memset(message->header->pathname, 0, MAX_PATH);
+    message->header->data_size = 0;
+    message->data = NULL;
+
+    return message;
+}
+
+static inline void destroymsg(msg_t *message) {
+#if DEBUG
+    printf("distruggo il messaggio %d: %s\n", message->header->code, message->header->pathname);
+    fflush(stdout);
+#endif
+    free(message->header);
+    if (message->data) free(message->data);
+    free(message);
+}
 
 #endif //FILE_STORAGE_SERVER_PROTOCOL_H
